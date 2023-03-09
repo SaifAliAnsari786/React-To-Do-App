781a089b9061497163679a7ce11117d5af713be4
SaifAliAnsari786 <saifaliansari477@gmail.com>
2023-03-09 16:04:24 +0545

    Ract To Do App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+  console.log(todo);
+
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const todoDelete = (text) => {
+    const tododata = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(tododata);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Heading">To Do List</h1>
+      <div className="input-warpper">
+        <input
+          type="text"
+          name="todo"
+          value={todo}
+          placeholder="Create new Task"
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button className="add-btn" onClick={addTodo}>
+          Add 
+        </button>
+      </div>
+      <div className="container">
+        {todos?.length > 0 ? (
+          <ul className="todo-list">
+            {todos.map((todo, index) => (
+              <div className="todo">
+                <li key={index}> {todo} 
+
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    todoDelete(todo);
+                  }}
+                >
+                  Delete
+                </button>
+                </li>
+              </div>
+            ))}
+          </ul>
+        ) : (
+          <div className="empty">
+            <p>No task found</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
